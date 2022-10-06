fc1d04206e82760e508a5736ff017848c57b80bd
HyejinUm <uhj1993@naver.com>
2022-10-06 23:18:18 +0900

    Fix: 구현사항대로ReactDOM.createRoot(container) 으로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import Root from './pages/Root';
 import About from './pages/About';
-import Router from './components/Router';
-import Route from './components/Route';
+import Router, { Route } from './components/Router';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const container = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(container).render(
   <Router>
-    <Route path='/' component={<Root />} />
-    <Route path='/about' component={<About />} />
+    <Route path="/" component={<Root />} />
+    <Route path="/about" component={<About />} />
   </Router>
 );
