f2c4779b932a0e073fb066be9f1f46c21211472c
HyejinUm <uhj1993@naver.com>
2022-10-06 19:05:30 +0900

    Feat: Rotuer, Route Component main.tsx에 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import Root from './pages/Root';
+import About from './pages/About';
+import Router from './components/Router';
+import Route from './components/Route';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    {/* <Router>
-      <Route path="/" component={<Root />} />
-      <Route path="/about" component={<About />} />
-    </Router> */}
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <Route path='/' component={<Root />} />
+    <Route path='/about' component={<About />} />
+  </Router>
 );
