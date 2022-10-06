89c964aaaa2bd3427197ef56a07b7dd3b6327b71
HyejinUm <uhj1993@naver.com>
2022-10-06 19:05:10 +0900

    Feat: (#2) Rotuer, Route Component

--- src/components/Route/index.tsx
+++ src/components/Route/index.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+// https://github.com/remix-run/react-router/blob/main/packages/react-router/lib/components.tsx#L263
 
-const Route = ({path, component}) => {
-    return (
-        
-    )
+export interface PathRouteProps {
+  component?: React.ReactNode | null;
+  path: string;
+}
+
+const Route = ({ component }: PathRouteProps): React.ReactElement | null => {
+  return <>{component}</>;
 };
 
 export default Route;
--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,5 +1,66 @@
-import React from "react";
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  createContext,
+  ReactElement,
+} from 'react';
+import NotFound from '../../pages/NotFound';
 
-const Router = ({}) => {};
+interface RouterProps {
+  basename?: string;
+  children?: ReactElement<{ path: string }> | ReactElement<{ path: string }>[];
+}
+
+export const RouteContext = createContext({
+  routename: '/',
+  setRoutename: (path: string) => {},
+});
+
+const Router = ({
+  basename = '/',
+  children,
+}: RouterProps): React.ReactElement | null => {
+  const [routename, setRoutename] = useState(basename);
+  const [renderPage, setRenderPage] = useState(children);
+
+  const routeContextValue = useMemo(
+    () => ({
+      routename,
+      setRoutename,
+    }),
+    [routename, setRoutename]
+  );
+
+  useEffect(() => {
+    if (!Array.isArray(children)) {
+      const isMatchPathname = children?.props.path === window.location.pathname;
+      setRenderPage(isMatchPathname ? children : <NotFound />);
+      return;
+    }
+
+    const matchPathname = children.find(
+      ({ props }) => props.path === window.location.pathname
+    );
+
+    window.addEventListener('popstate', () => {
+      setRoutename(window.location.pathname);
+    });
+
+    setRenderPage(matchPathname);
+
+    return () => {
+      window.removeEventListener('popstate', () => {
+        setRoutename(window.location.pathname);
+      });
+    };
+  }, [routename]);
+
+  return (
+    <RouteContext.Provider value={routeContextValue}>
+      {renderPage}
+    </RouteContext.Provider>
+  );
+};
 
 export default Router;
