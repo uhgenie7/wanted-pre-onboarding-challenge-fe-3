ea4fa2e2b2c0683c712b927760551918e9e10705
HyejinUm <uhj1993@naver.com>
2022-10-07 18:59:22 +0900

    Config: Vite 설정 중...

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/wanted-pre-onboarding-challenge-fe-3',
   plugins: [react()],
-  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        root: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
