53988a6c49eb91ea646825d4cb839932ec64f6c7
HyejinUm <uhj1993@naver.com>
2022-10-07 18:10:11 +0900

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wanted-pre-onboarding-challenge-fe-3/',
   plugins: [react()],
 });
