6a08ba47e2e4a3abef92492147789d1d367c4727
HyejinUm <uhj1993@naver.com>
2022-10-08 00:33:46 +0900

    Issue: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        root: resolve(__dirname, 'index.html'),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
   },
