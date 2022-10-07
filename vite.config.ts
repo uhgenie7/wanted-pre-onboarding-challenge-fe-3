cabd85acc705a75144def88709ccfacfd56fb398
HyejinUm <uhj1993@naver.com>
2022-10-07 17:16:54 +0900

    Add: dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: '/wanted-pre-onboarding-challenge-fe-3/',
+  plugins: [react()],
+});
